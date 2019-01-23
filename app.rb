bff9648a1641fb19db3e4ee0a3552c146631a340
Elise Phan <aliceconnic@gmail.com>
2019-01-23 05:07:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,9 +20,8 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-   "#{@num1}*#{@num2}"
+   total = params[:num1] * params[:num2]
+   "#{total}"
   end
 
 end
